bcd4393b9f8e208e607da1a4d925daac8a0dfa9c
sanya-padalko <apadalko90@gmail.com>
2025-09-17 01:55:35 +0300

    Update errors.h
    Коды ошибок в нужном порядке + теперь можно легче добавлять новые ошибки

--- errors.h
+++ errors.h
@@ -4,10 +4,11 @@
 #define my_assert(comp, code_err) if (comp) {print_err_message(__FILE__, __FUNCTION__, __LINE__, code_err);exit(0);}
 
 enum ERRORS {
-    NOTHING        = 3,
-    NAN_ERR        = 1,
-    NULLPTR        = 0,
-    INFINITY_DIGIT = 2
+    NULLPTR        =   0,
+    NAN_ERR        =   1,
+    INFINITY_DIGIT =   2,
+    FILE_ERR       =   3,
+    NOTHING        = 100
 };
 
 void print_err_message(const char *file_name, const char* func_name, const int n_line,
