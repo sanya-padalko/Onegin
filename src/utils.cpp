6f138c3f3f690aec1ac575583fdc6bc60cfffd9d
sanya-padalko <apadalko90@gmail.com>
2025-09-19 03:57:35 +0300

    Update utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,24 +1,30 @@
 #include "../inc/utils.h"
 
-void next(char** ptr) {
-    my_assert(!ptr, NULLPTR);
-    my_assert(!(*ptr), NULLPTR);
+void next_ptr(char** ptr, ERRORS* code_error) {
+    my_assert(ptr, NULLPTR, );
+    my_assert(*ptr, NULLPTR, );
 
     while (**ptr != '\n' && !isalpha(**ptr))
         ++*ptr;
 }
 
-int get_file_size(char* file_name) {
-    my_assert(!file_name, NULLPTR);
+void prev_ptr(char** ptr, char* stop) {
+    while (*ptr > stop && !isalpha(**ptr))
+        --*ptr;
+}
+
+int get_file_size(char* file_name, ERRORS* code_error) {
+    my_assert(file_name, NULLPTR, 0);
 
     struct stat file_stat; 
-    my_assert(stat(file_name, &file_stat), FILE_ERR);
+    int stat_result = stat(file_name, &file_stat);
+    my_assert(!stat_result, FILE_ERR, 0);
 
     return file_stat.st_size;
 }
 
-int count_strings(char* buf, int file_size) {
-    my_assert(!buf, NULLPTR);
+int count_strings(char* buf, int file_size, ERRORS* code_error) {
+    my_assert(buf, NULLPTR, 0);
     
     int n_strings = 0;
     for (int i = 0; i < file_size; ++i) {
@@ -28,7 +34,11 @@ int count_strings(char* buf, int file_size) {
     return n_strings;
 }
 
-void separate_strings(char* buf, char** mas_point, STR* mas_str, int file_size) {
+void separate_strings(char* buf, char** mas_point, STR* mas_str, int file_size, ERRORS* code_error) {
+    my_assert(buf, NULLPTR, );
+    my_assert(mas_point, NULLPTR, );
+    my_assert(mas_str, NULLPTR, );
+
     char* ptr = buf;
     *mas_point++ = ptr;
     mas_str->start = ptr++;
@@ -39,28 +49,4 @@ void separate_strings(char* buf, char** mas_point, STR* mas_str, int file_size) 
             mas_str->start = *mas_point++ = ptr;
         }
     }
-}
-
-void input_file(char* buf, char* file_name, int file_size) {
-    my_assert(!buf, NULLPTR);
-    my_assert(!file_name, NULLPTR);
-
-    FILE* fp = fopen(file_name, "rb");
-    my_assert(!fp, NULLPTR);
-
-    printf("%d symnols read\n", fread(buf, sizeof(char), file_size, fp));
-    
-    fclose(fp);
-}
-
-void output_file(char* buf, char* file_name, int file_size) {
-    my_assert(!buf, NULLPTR);
-    my_assert(!file_name, NULLPTR);
-
-    FILE* fp = fopen(file_name, "wb");
-    my_assert(!fp, NULLPTR);
-
-    fprintf(stderr, "%d symbols printed\n", fwrite(buf, sizeof(char), file_size, fp));
-    
-    fclose(fp);
 }
