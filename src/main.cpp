5ac7b4e99977424f61bfbb6bb13687613f45540e
sanya-padalko <apadalko90@gmail.com>
2025-09-19 03:57:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,63 +2,28 @@
 #include "../inc/utils.h"
 #include "../inc/strings.h"
 #include "../inc/sorting.h"
+#include "../inc/fwork.h"
 
 int main() {
 
-    // Naming
+    ERRORS* code_error = (ERRORS*)calloc(1, sizeof(ERRORS));
+    *code_error = NOTHING;
 
-    char* file_name     =          "Onegin.txt";
-    char* file_original = "Original_Onegin.txt";
-    char* file_sorting  =  "Sorting_Onegin.txt";
+    File main_file;
+    main_file.name = "Onegin.txt";
 
-    // Reading file
-
-    int file_size = get_file_size(file_name);
-
-    char* buf = (char*)calloc(file_size, sizeof(char));
-    my_assert(!buf, NULLPTR);
-
-    input_file(buf, file_name, file_size);
-
-    int n_strings = count_strings(buf, file_size);
+    File sorting_file;
+    sorting_file.name =  "Sorting_Onegin.txt";
     
-    fprintf(stderr, "\033[32m%d symbols,   %d strings\033[0m\n", file_size, n_strings);
-
-    // Finding strings
+    read_information(&main_file, code_error);
     
-    char** mas_point = (char**)calloc(n_strings + 1, sizeof(char*));
-    my_assert(!mas_point, NULLPTR);
+    print_information(&main_file, code_error);
 
-    STR* mas_str = (STR*)calloc(n_strings + 1, sizeof(STR));
-    my_assert(!mas_str, NULLPTR);
+    select_strings(&main_file, code_error);
 
-    separate_strings(buf, mas_point, mas_str, file_size);
+    file_sorting(&main_file, code_error);
 
-    // Sorting
+    print_sorting(&main_file, &sorting_file, code_error);
 
-    bubble_sort(mas_point, n_strings);
-
-    qsort(mas_str, n_strings, sizeof(STR), &compar);
-
-    // Output text
-
-    output_file(buf, file_original, file_size);
-
-    // Output sorting text
-
-    FILE* file_answer = fopen(file_sorting, "wb");
-    
-    for (int i = 0; i < n_strings; ++i) {
-        my_puts(mas_str[i].start, file_answer);
-    }
-
-    for (int i = 0; i < 31; ++i) {
-        my_puts("", file_answer);
-        if (i == 15) 
-            my_puts("---------------------------------------------------------------------------\n", file_answer);
-    }
-
-    for (int i = 0; i < n_strings; ++i) {
-        my_puts(mas_point[i], file_answer);
-    }
+    print_original(&main_file, &sorting_file, code_error);
 }
