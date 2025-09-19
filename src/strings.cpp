2bacb9af8a4daecb62506c5147b3e3a465872a28
sanya-padalko <apadalko90@gmail.com>
2025-09-19 03:57:33 +0300

    Update strings.cpp

--- src/strings.cpp
+++ src/strings.cpp
@@ -1,8 +1,8 @@
 #include "../inc/strings.h"
 
-void my_puts(const char* str, FILE* stream) {
-    my_assert(!str, NULLPTR);
-    my_assert(!stream, NULLPTR);
+void my_puts(const char* str, FILE* stream, ERRORS* code_error) {
+    my_assert(str, NULLPTR, );
+    my_assert(stream, NULLPTR, );
     
     while (*str != '\n' && *str != '\0')
         fprintf(stream, "%c", *str++);
@@ -10,8 +10,19 @@ void my_puts(const char* str, FILE* stream) {
     fputc('\n', stream);
 }
 
-size_t my_strlen(const char* str) {
-    my_assert(!str, NULLPTR);
+void print_empty_strings(int x, FILE* stream, ERRORS* code_error) {
+    my_assert(stream, NULLPTR, );
+    my_assert(code_error, NULLPTR, );
+
+    for (int i = 0; i < x; ++i) {
+        my_puts("", stream, code_error);
+        if (i == (x / 2))
+            my_puts("----------------------------------------------------------------------\n", stream, code_error);
+    }
+}
+
+size_t my_strlen(const char* str, ERRORS* code_error) {
+    my_assert(str, NULLPTR, 0);
 
     int len = 0;
 
